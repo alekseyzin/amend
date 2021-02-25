8b98be738906005120d9a2cbefc9199f5fd17763
Zinchenko Aleksey <a.zinchenko@andersenlab.com>
2021-02-25 14:40:31 +0200

    first

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ const holder = document.querySelector('.holder')
 const button = document.querySelector('#button');
 let isHorizontal = false;
 
+let leftHeight = "40%"
+let leftWidth = "60%"
+
+
 button.addEventListener('click', () => {
     holder.classList.toggle('column')
-    right.classList.toggle('column-right')
-    left.classList.toggle('column-left')
     resize.classList.toggle('column-resize')
     isHorizontal = !isHorizontal;
+    if (isHorizontal) {
+        left.style.width = '100%'
+        left.style.height = leftHeight
+        right.style.height = 'auto'
+    } else {
+        left.style.height = "100%"
+        right.style.height = "100%"
+        left.style.width = leftWidth
+    }
+
 })
 
 resize.addEventListener('mousedown', (event) => {
@@ -25,11 +37,13 @@ resize.addEventListener('mousedown', (event) => {
         if(!isHorizontal) {
             if (right.offsetWidth >= 200 || (right.offsetWidth < 200 && startX - currentX > 0)) {
                 left.style.width = left.offsetWidth + currentX - startX + 'px';
+                leftWidth = left.offsetWidth + currentX - startX + 'px';
             }
             startX = currentX
         } else {
             if (right.offsetHeight >= 200 || (right.offsetHeight < 200 && startY - currentY > 0)) {
                 left.style.height = left.offsetHeight + currentY - startY + 'px';
+                leftHeight = left.offsetHeight + currentY - startY + 'px';
             }
             startY = currentY
         }
@@ -37,8 +51,6 @@ resize.addEventListener('mousedown', (event) => {
 
         return false;
     }
-
-    // moveMouseEvent = document.addEventListener('mousemove', resizeBlocks)
     document.onmousemove = resizeBlocks
 })
 
